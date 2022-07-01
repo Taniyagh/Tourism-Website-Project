47e1ce3ea33a2e2ee3177b8667ab22d09270503e
atiyegh <72501292+atiyegh@users.noreply.github.com>
2022-07-01 14:09:58 +0430

    Create Home.js

---
+++ src/Components/Pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import HeroSection from '../HeroSection/HeroSection'
+import Cards from '../Cards/Cards'
+function Home() {
+  return (
+    <React.Fragment>
+        <HeroSection/>
+        <Cards/>
+    </React.Fragment>
+  )
+}
+
+export default Home
